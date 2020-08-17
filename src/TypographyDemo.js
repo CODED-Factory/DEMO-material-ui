e6ff26964eae951e4282bc861bd8b15f1841bdb9
Khalid AlMohammed <kalmohammed56@gmail.com>
2020-08-17 19:29:24 +0300

    typographyDemo matches the readme file

--- src/TypographyDemo.js
+++ src/TypographyDemo.js
@@ -4,8 +4,12 @@ import { Typography } from "@material-ui/core";
 const TypographyDemo = () => {
   return (
     <div>
-      <Typography variant="h3" color="primary">
-        Here is some text
+      <Typography align="center">Any text here</Typography>
+      <Typography align="left" variant="h1">
+        Any text here
+      </Typography>
+      <Typography align="right" variant="h3" gutterBottom color="primary">
+        Any text here
       </Typography>
     </div>
   );
